4c54ee08e82371e5b793691080be89d8b48df588
nixena <nikanozadze1010@gmail.com>
2023-05-22 23:55:19 +0400

    feat: done file upload challenge

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,31 @@
 <script>
+  import Card from "./Card.svelte";
+
+  let price = 50;
+  let description = "";
+  let image;
+
+  const imageHandler = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = function (event) {
+        image = event.target.result;
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
 </script>
 
-<main />
+<main>
+  <input type="text" bind:value={price} />
+  <input type="text" bind:value={description} />
+  <input type="file" on:input={imageHandler} />
+  <br />
+  <Card {price} {description} {image} />
+</main>
 
 <style>
 </style>
---
+++ src/Card.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let price;
+  export let description;
+  export let image;
+</script>
+
+<main>
+  <div>
+    <h2>product price is {price}</h2>
+    {description}
+    <img src={image} alt="" />
+  </div>
+</main>
